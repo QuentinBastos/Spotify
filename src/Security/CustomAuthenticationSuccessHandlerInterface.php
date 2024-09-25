b241020e0d6b1f83e5970e0fa176c7b75426ba97
p2110156 <quentin.bastos@etu.univ-lyon1.fr>
2024-09-25 21:10:22 +0200

    feat(login): add redirection login / logout

---
+++ src/Security/CustomAuthenticationSuccessHandlerInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        // Redirect to a chosen page, e.g., the homepage
+        return new RedirectResponse($this->router->generate('app_tracks'));
+    }
+}
