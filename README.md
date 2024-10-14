94be16e700c0c2080a6faf465c2e67d9db03a253
p2110156 <quentin.bastos@etu.univ-lyon1.fr>
2024-10-14 09:35:02 +0200

    feat(favorite): entity + migration

--- README.md
+++ README.md
@@ -11,3 +11,17 @@
 - The database is use in local with wamp or xamp (on phpmyadmin 5.2.1)
 
 ```
+
+# RUN THE DATABASE IF PROBLEM WITH WAMP : 
+```txt
+- database can be run with wamp
+- if you have a problem with that like error : 
+ ERROR 2059 (HY000): Authentication plugin 'auth_gssapi_client' cannot be loaded: 
+ The specified module could not be found.
+ 
+- you can use the command : 
+    - net stop MariaDB
+    - net start MySQL80
+
+because there is a conflict with both of database
+```
---
+++ src/Entity/Favorite.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FavoriteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FavoriteRepository::class)]
+#[ORM\Table(name: 'favorite')]
+class Favorite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(targetEntity: Track::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Track $track = null;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): void
+    {
+        $this->user = $user;
+    }
+
+    public function getTrack(): ?Track
+    {
+        return $this->track;
+    }
+
+    public function setTrack(?Track $track): void
+    {
+        $this->track = $track;
+    }
+}
